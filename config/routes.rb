0159afe0ebde63ef4456816c93dcb61bd21760ca
Eric Ying <eric99ying@gmail.com>
2020-12-17 20:07:49 -0800

    Create home page and recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # Redirect root page to home page
+    root :to => "static_pages#home"  
+
+    # Routes for static pages like home page...
+    get 'static_pages/home'
+
+    # RESTFUL routes for recipes
+    resources :recipes
+
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
