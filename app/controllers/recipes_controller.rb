56f240807dcada94b5ac63c89de685fb565c6cb0
Eric Ying <eric99ying@gmail.com>
2021-01-20 15:22:18 -0800

    Added page for each recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,14 @@ class RecipesController < ApplicationController
     def index
         @recipes = Recipe.all_recipes
     end
+
+    def show
+        if Recipe.exists?(:id => params[:id])
+            @recipe = Recipe.find(params[:id])
+        else 
+            # If recipe does not exist, redirect to 404 not found page
+            @recipe = nil
+            raise ActionController::RoutingError.new('Not Found')
+        end
+    end
 end
